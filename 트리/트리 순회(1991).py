948a6d39a3e24d0e1a96f804ba7587c401c59bbb
dpfprtus <dpfprtus@gmail.com>
2023-08-30 17:38:53 +0900

    틀림

---
+++ 트리/트리 순회(1991).py
@@ -0,0 +1,29 @@
+n = int(input())
+tree = {}
+
+def preorder(root):
+    if root != ".":
+        print(root,end= '')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+        
+def middleorder(root):
+    if root != ".":
+        middleorder(tree[root][0])
+        print(root,end='')
+        middleorder(tree[root][1])
+def postorder(root):
+    if root != ".":
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+for _ in range(n):
+    a,b,c = input().split()
+    tree[a] = [b,c]
+    
+preorder("A")
+print()
+middleorder("A")
+print()
+postorder("A")
