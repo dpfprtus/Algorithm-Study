385e07defe4508f91c72ea70dc1918ad44af515d
dpfprtus <dpfprtus@gmail.com>
2023-09-15 23:25:54 +0900

    또틀림

--- 자료구조/쇠막대기(10799).py
+++ 자료구조/쇠막대기(10799).py
@@ -1,17 +1,22 @@
-a = input()
+import sys
+input = sys.stdin.readline
+
 stack = []
 result = 0
-for i in range(len(a)):
-    if a[i] == "(":
-        stack.append(i)
-    elif a[i] == ")":
-        if a[i-1] == "(":
+tmp = 0
+s = input()
+
+for i in range(len(s)):
+    if s[i] == "(":
+        stack.append(s[i])
+    elif s[i] == ")":
+        
+        if s[i-1] == "(":
             stack.pop()
             result += len(stack)
         else:
-            stack.pop() #))이런식이면 1만 추가하면된다.
             result += 1
-            
+            stack.pop()
 print(result)
-            
-    
+        
+        
