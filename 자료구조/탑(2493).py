50e0d3544168757628de32124316fee100dab8ee
dpfprtus <dpfprtus@gmail.com>
2023-09-17 20:25:57 +0900

    또틀림

--- 자료구조/탑(2493).py
+++ 자료구조/탑(2493).py
@@ -1,20 +1,21 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-top = int(input())
-razer = list(map(int,stdin.readline().rstrip().split()))
+n = int(input())
+top = list(map(int,input().split()))
 result = []
 answer = []
-
-#완전 탐색으로 하니까 시간초과뜸.
-for i in range(top):
-    while(result):
-        if result[-1][1] > razer[i]:
-            answer.append(result[-1][0] + 1)
+for i in range(n):
+    while result:
+        if result[-1][1] > top[i]:
+            answer.append(result[-1][0]+1)
             break
         else:
             result.pop()
     if not result:
         answer.append(0)
-    result.append([i,razer[i]])
-    
+    result.append([i,top[i]])
 print(' '.join(map(str,answer)))
+
+            
+        
