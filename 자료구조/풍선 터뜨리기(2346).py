9eba72153e834eb9324bf4d6ba1162f8a7a7a047
dpfprtus <dpfprtus@gmail.com>
2023-09-18 11:21:42 +0900

    틀림(다시품)

--- 자료구조/풍선 터뜨리기(2346).py
+++ 자료구조/풍선 터뜨리기(2346).py
@@ -1,20 +1,15 @@
 from collections import deque
 
-num = int(input())
-bollon = deque(enumerate(map(int,input().split()))) #인덱스를 저장하기 위해 enumerate사용 (0,1),(1,2)...
-cnt = 0
-idx = 0
+n = int(input())
+ballon = deque(enumerate(map(int,input().split())))
 result = []
-while(bollon):
-    index,cnt = bollon.popleft()
-    result.append(index+1)
-    if cnt > 0:
-        bollon.rotate(-(cnt-1))
+for i in range(n):
+    first = ballon.popleft()
+    result.append(first[0]+1)
+    if first[1] > 0:
+        ballon.rotate(-first[1]+1)
     else:
-        bollon.rotate(-cnt)
+        ballon.rotate(-first[1])
+    print(ballon)
+        
 print(' '.join(map(str,result)))
-
-    
-            
-        
-        
