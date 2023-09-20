14b33db81574e4c76122881a443aacb7a5a837d2
suyong choi <dpfprtus@gmail.com>
2023-09-20 14:20:11 +0900

    또틀림

--- 자료구조2/이중 우선순위 큐(7662).py
+++ 자료구조2/이중 우선순위 큐(7662).py
@@ -1,37 +1,45 @@
-import heapq
-from sys import stdin
+import sys,heapq
+input = sys.stdin.readline
 
 t = int(input())
 
+
 for _ in range(t):
     k = int(input())
-    visited = [False]*1_000_001
-    heap = []
-    heap2 = []
+    max_heap = []
+    min_heap = []
+    visited = [False] * 1000001
     for i in range(k):
-        a,b = stdin.readline().rstrip().split()
+        a,b = input().rstrip().split()
         if a == "I":
-            heapq.heappush(heap2,(-int(b),i)) #최댓값
-            heapq.heappush(heap,(int(b),i)) #최솟값
+            heapq.heappush(max_heap,(-int(b),i))
+            heapq.heappush(min_heap,(int(b),i))
             visited[i] = True
         elif a == "D":
-            if int(b) == -1:
-                while heap and not visited[heap[0][1]]:
-                    heapq.heappop(heap)
-                if heap:
-                    visited[heap[0][1]] = False
-                    heapq.heappop(heap)
-                  
-            elif int(b) == 1:
-                while heap2 and not visited[heap2[0][1]]:
-                    heapq.heappop(heap2)
-                if heap2:
-                    visited[heap2[0][1]] = False
-                    heapq.heappop(heap2)
-    while heap and not visited[heap[0][1]]:heapq.heappop(heap)
-    while heap2 and not visited[heap2[0][1]]:heapq.heappop(heap2)
-    if heap and heap2:
-        print(-heap2[0][0],heap[0][0])
-        
+            if int(b) == 1: #최댓값 삭제
+               
+                while min_heap and visited[min_heap[0][1]] != True:
+                    heapq.heappop(min_heap)
+                if max_heap:
+                    visited[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+                     
+              
+            else: #최솟값 삭제
+                
+                while max_heap and visited[max_heap[0][1]] != True:
+                    heapq.heappop(max_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
+                    
+           
+    while max_heap and visited[max_heap[0][1]] != True:
+        heapq.heappop(max_heap)
+    while min_heap and visited[min_heap[0][1]] != True:
+        heapq.heappop(min_heap)
+    if min_heap and max_heap:
+        print(-max_heap[0][0],min_heap[0][0])
     else:
         print("EMPTY")
+    
