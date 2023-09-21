341ee1323b772e7c40e355ebcacaa20d84fc6794
suyong choi <dpfprtus@gmail.com>
2023-09-21 13:52:53 +0900

    틀림(다시품)

--- 트리/단절점과 단절선(14675).py
+++ 트리/단절점과 단절선(14675).py
@@ -1,21 +1,24 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
 n = int(input())
 tree = [[] for _ in range(n+1)]
-
 for _ in range(n-1):
-    a,b = map(int,stdin.readline().split())
+    a,b = map(int,input().split())
     tree[a].append(b)
     tree[b].append(a)
-    
-q =int(input())
 
-for _ in range(q):
-    t,k = map(int,stdin.readline().split())
+
+
+
+q = int(input())
+for i in range(q):
+    t,k = map(int,input().split())
     if t == 1:
         if len(tree[k]) < 2:
             print("no")
         else:
             print("yes")
-    elif t==2:
+    elif t == 2:
         print("yes")
+
