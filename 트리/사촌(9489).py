49bf00ce08d54119f22371934dc131673489f547
dpfprtus <dpfprtus@gmail.com>
2023-09-22 18:45:37 +0900

    또틀림

--- 트리/사촌(9489).py
+++ 트리/사촌(9489).py
@@ -1,20 +1,21 @@
-from collections import deque,defaultdict
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
 
 while True:
-
     n,k = map(int,input().split())
     if n == 0 and k == 0:
         break
     num = list(map(int,input().split()))
-    idx = 0
     parent = defaultdict(int)
+    index = 0
     for i in range(1,n):
-        parent[num[i]] = num[idx]
-        if i < n-1 and num[i+1] - num[i] != 1:
-            idx +=1
+        parent[num[i]] = num[index]
+        if i < n-1 and num[i+1]-num[i] != 1:
+            index += 1
     cnt = 0
-    if parent[parent[k]]:
-        for i in num:
-            if parent[parent[i]] == parent[parent[k]] and parent[i] != parent[k]:
-                cnt += 1
+    for i in num:
+        if parent[parent[k]] and parent[parent[k]] == parent[parent[i]] and parent[k] != parent[i]:
+           cnt += 1 
     print(cnt)
+            
