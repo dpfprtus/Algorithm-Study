bb133bda53318b0aafaec69c31a461dc0be62b47
dpfprtus <dpfprtus@gmail.com>
2023-09-23 14:25:23 +0900

    또틀림

--- 트리/이진 검색 트리(5639).py
+++ 트리/이진 검색 트리(5639).py
@@ -1,25 +1,24 @@
 import sys
-sys.setrecursionlimit(10 ** 6)
+from sys import setrecursionlimit
+setrecursionlimit(10**9)
 input = sys.stdin.readline
-pre = []
-
-while True:
-    try:
-        pre.append(int(input()))
-    except:
-        break
+tree = []
 
 def post(start,end):
-    if start > end:
-        return
-    mid = end + 1
+    if start > end: return
+    mid = end+1
     for i in range(start+1,end+1):
-        if pre[i] > pre[start]:
+        if tree[i] > tree[start]:
             mid = i
             break
     post(start+1,mid-1)
     post(mid,end)
-    print(pre[start])
+    print(tree[start])
+    
 
-post(0,len(pre)-1)
-
+while True:
+    try:
+        tree.append(int(input()))
+    except:
+        break
+post(0,len(tree)-1)
