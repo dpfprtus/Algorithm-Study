f36a853cc0e440549890c4cfb992ab1db6969ab3
dpfprtus <dpfprtus@gmail.com>
2023-09-28 23:35:47 +0900

    또틀림

--- 트리/트리(4256).py
+++ 트리/트리(4256).py
@@ -1,9 +1,8 @@
 def makePost(preorder,inorder):
-    if not preorder:return
+    if not preorder: return
     root = preorder[0]
     index = inorder.index(root)
-    
-    makePost(preorder[1:index+1],inorder[:index])
+    makePost(preorder[1:index+1],inorder[:index+1])
     makePost(preorder[index+1:],inorder[index+1:])
     print(root,end=' ')
 
