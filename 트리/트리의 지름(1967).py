ee565a78e9a4708191b196464079665ab299dbd6
suyong choi <dpfprtus@gmail.com>
2023-10-09 22:50:11 +0900

    또틀림

--- 트리/트리의 지름(1967).py
+++ 트리/트리의 지름(1967).py
@@ -1,26 +1,27 @@
 from sys import setrecursionlimit
-setrecursionlimit(10 ** 9)
+setrecursionlimit(10**9)
+
 n = int(input())
 tree = [[] for _ in range(n+1)]
-
 for _ in range(n-1):
-    a,b,w = map(int,input().split())
-    tree[a].append((b,w))
-    tree[b].append((a,w))
+    parent, child,w = map(int,input().split())
+    tree[parent].append([child,w])
+    tree[child].append([parent,w])
 
-a = [0]
 def dfs(node,we):
-    for node,w in tree[node]:
-        if distance[node] == -1:
-            distance[node] = we+w
-            dfs(node,we+w)
-            
+    for i in tree[node]:
+        a,b = i
+        if distance[a] == -1:
+            distance[a] =  we+b
+            dfs(a,we+b)
+
+
 distance = [-1] * (n+1)
 distance[1] = 0
+dfs(1,0)
 
-result = dfs(1,0)
 start = distance.index(max(distance))
 distance = [-1] * (n+1)
 distance[start] = 0
-result2 = dfs(start,0)
+dfs(start,0)
 print(max(distance))
