81660f778cfaa99aebbd1cdb34bcd98ca9a180cb
suyong choi <dpfprtus@gmail.com>
2023-10-31 13:07:49 +0900

    틀림

---
+++ 수학/최대공약수와 최소공배수(2609).py
@@ -0,0 +1,15 @@
+a,b = map(int,input().split())
+
+def gcd(a,b):
+    if a > b:
+        while(b > 0):
+            a ,b= b, a%b
+        return a
+    else:
+        while(a > 0):
+            b,a = a, b %a
+        return b
+
+print(gcd(a,b))
+print(int(a*b / gcd(a,b)))
+
