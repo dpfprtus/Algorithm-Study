1520b392c3c749e3f46e16ea50a48e8a00d2af7e
suyong choi <dpfprtus@gmail.com>
2024-01-12 14:05:17 +0900

    다시품:

--- 그리디/센서(2212).py
+++ 그리디/센서(2212).py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
 k = int(input())
-position = list(map(int,input().split()))
-position.sort()
-ans = []
 
+point = list(map(int,input().split()))
+point.sort()
+ans = []
 for i in range(n-1):
-    ans.append(position[i+1]-position[i])
+    ans.append(abs(point[i+1]-point[i]))
 ans.sort()
 print(sum(ans[:n-k]))
-
