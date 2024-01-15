8718ca477f00fda0a3e33e28f5a25b28a73898fc
suyong choi <dpfprtus@gmail.com>
2024-01-15 17:11:43 +0900

    다시품

--- 그리디/행복 유치원(13164).py
+++ 그리디/행복 유치원(13164).py
@@ -1,14 +1,7 @@
-from itertools import combinations
-
 n,k = map(int,input().split())
-height = list(map(int,input().split()))
-price = 0
+key = list(map(int,input().split()))
 ans = []
 for i in range(1,n):
-    ans.append(height[i]-height[i-1])
-print(ans)
-ans.sort(reverse=True)
-print(ans)
-print(sum(ans[k-1:]))
-
-
+    ans.append(key[i]-key[i-1])
+ans.sort()
+print(sum(ans[:n-k]))
