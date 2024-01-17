6c1269e1cc0b8d5c95f4719f453f50e73be744d3
suyong choi <dpfprtus@gmail.com>
2024-01-17 18:28:41 +0900

    틀림

---
+++ DP1/설탕배달(2839).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = [-1] * 5001
+d[3] = d[5] = 1
+for i in range(6,n+1):
+    if i % 5 == 0:
+        d[i] = d[i-5] + 1
+    elif i % 3 == 0:
+        d[i] = d[i-3] + 1
+    elif d[i-3] > 0 and d[i-5]>0:
+        d[i] = min(d[i-3],d[i-5]) + 1
+print(d[n])
