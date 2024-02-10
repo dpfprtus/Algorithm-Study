c4027b985fa0083fdc12deeeb92926611011a738
dpfprtus <dpfprtus@gmail.com>
2024-02-10 22:35:11 +0900

    틀림

--- DP1/small징검다리건너기(22869).py
+++ DP1/small징검다리건너기(22869).py
@@ -2,7 +2,7 @@ n,k = map(int,input().split())
 s = list(map(int,input().split()))
 dp = [0] * n
 dp[0] = 1
-for i in range(n-1):
+for i in range(n-1  ):
     if dp[i] == 1:
         for j in range(i+1,n):
             if dp[j] == 0 and k >= (j-i) * (1+abs(s[j]-s[i])):
