ab525888b27527c2bc94995cd854d1697471fd03
suyong choi <dpfprtus@gmail.com>
2024-02-14 16:56:22 +0900

    다시품

--- DP1/1,2,3더하기(9095).py
+++ DP1/1,2,3더하기(9095).py
@@ -1,11 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
+
 for _ in range(t):
     n = int(input())
-    cnt = 0
-    dp = [0] * 11
+    dp = [0]*(12)
     dp[1] = 1
     dp[2] = 2
     dp[3] = 4
-    for i in range(4,n+1):
-        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+    for j in range(4,n+1):
+        dp[j] = dp[j-1]+dp[j-2]+dp[j-3]
     print(dp[n])
+
+
