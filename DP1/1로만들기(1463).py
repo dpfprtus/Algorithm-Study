f661d5c3aa2a1b01f549b96b039ca607f22d847b
suyong choi <dpfprtus@gmail.com>
2024-02-15 11:24:54 +0900

    다시품

--- DP1/1로만들기(1463).py
+++ DP1/1로만들기(1463).py
@@ -1,13 +1,16 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-cnt = 0
-dp = [0] * (n+1)
-for i in range(2,n+1):
-    dp[i] = dp[i-1] + 1
-    if i % 3 == 0:
-        dp[i] = min(dp[i],dp[i//3]+1)
-    if i % 2 == 0:
-        dp[i] = min(dp[i],dp[i//2]+1)
+
+dp = [0]*(1000001)
+dp[2] = 1
+dp[3] =1 
+for i in range(4,n+1):
+    if i%3 == 0 and i%2 == 0:
+        dp[i] = min(dp[i//3]+1,dp[i//2]+1,dp[i-1]+1)
+    elif i % 3 == 0:
+        dp[i] = min(dp[i//3]+1,dp[i-1]+1)
+    elif i % 2 == 0:
+        dp[i] = min(dp[i//2]+1,dp[i-1]+1)
+     
+    else:
+        dp[i] = dp[i-1]+1
 print(dp[n])
