75d338e59e3ccc6118e9e95a22224a22a43d9178
suyong choi <dpfprtus@gmail.com>
2024-02-16 10:42:43 +0900

    다시품

--- DP1/2xn타일링2(11727).py
+++ DP1/2xn타일링2(11727).py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-
-dp = [0] * 1001
-dp[0]=1
+dp = [0]*(1001)
 dp[1] = 1
-
-for i in range(2,n+1):
-    dp[i] = dp[i-1] + 2*dp[i-2]
-print(dp[n] % 10007)
+dp[2] = 3
+dp[3] = 5
+for i in range(4,n+1):
+    dp[i] = dp[i-2]*2 + dp[i-1]
+print(dp[n] % 10007) 
