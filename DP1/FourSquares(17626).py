fc4aaa996970fff6807dbe6f143c9490741d356b
suyong choi <dpfprtus@gmail.com>
2024-02-24 11:28:40 +0900

    또틀림

--- DP1/FourSquares(17626).py
+++ DP1/FourSquares(17626).py
@@ -1,12 +1,11 @@
-import sys,math
-input = sys.stdin.readline
+import math
 n = int(input())
-cnt = 0
 dp = [0,1]
 
 for i in range(2,n+1):
-    _min = 1e9
-    for j in range(1,int(math.sqrt(i))+1):
-        _min = min(_min,dp[i - j ** 2])
-    dp.append(_min+1)
+    mincount = 4
+    for j in range(int(math.sqrt(i)),0,-1):
+        mincount = min(mincount,dp[i-j**2]+1)
+    dp.append(mincount)
+
 print(dp[n])
