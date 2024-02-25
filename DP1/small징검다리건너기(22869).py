84ff16526d9f5c077aa6d2506f6abd7de0158194
suyong choi <dpfprtus@gmail.com>
2024-02-25 12:16:55 +0900

    또틀림

--- DP1/small징검다리건너기(22869).py
+++ DP1/small징검다리건너기(22869).py
@@ -1,10 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n,k = map(int,input().split())
 s = list(map(int,input().split()))
 dp = [0] * n
 dp[0] = 1
-for i in range(n-1  ):
+
+for i in range(n-1):
     if dp[i] == 1:
-        for j in range(i+1,n):
-            if dp[j] == 0 and k >= (j-i) * (1+abs(s[j]-s[i])):
+        for j in range(1,n):
+            if dp[j] == 0 and (j-i)*(1+abs(s[i]-s[j])) <= k:
                 dp[j] = 1
 print("YES" if dp[n-1] else "NO")
