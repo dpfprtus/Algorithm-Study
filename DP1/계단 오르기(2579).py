810c8dce6510725ec287cd9b407220790ff29641
dpfprtus <dpfprtus@gmail.com>
2024-02-26 22:42:28 +0900

    다시품

--- DP1/계단 오르기(2579).py
+++ DP1/계단 오르기(2579).py
@@ -1,16 +1,22 @@
 import sys
-inpurt = sys.stdin.readline
+input = sys.stdin.readline
 
 n = int(input())
-stair = [0] * (301)
-for i in range(1,n+1):
-    stair[i] = int(input())
+point = [0]+[int(input()) for _ in range(n)]
 
-
-dp = [0] * (301)
-dp[1] = stair[1]
-dp[2] = stair[1]+stair[2]
-dp[3] = max(stair[3]+dp[1],stair[3]+stair[2])
+dp = [0] * (n+1)
+if n == 1:
+    print(point[1])
+    exit()
+elif n == 2:
+    print(point[1] + point[2])
+    exit()
+elif n == 3:
+    print(max(point[1]+point[3], point[2]+point[3]))
+    exit()
+dp[1] = point[1]
+dp[2] = point[1] + point[2]
+dp[3] = max(point[1]+point[3], point[2]+point[3])
 for i in range(4,n+1):
-    dp[i] = max(stair[i]+stair[i-1]+dp[i-3],stair[i]+dp[i-2])
+    dp[i] = max(point[i]+dp[i-2],point[i]+point[i-1]+dp[i-3])
 print(dp[n])
