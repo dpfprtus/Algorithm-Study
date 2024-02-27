278566dbeed8a19ddab74c7ec1468722d450bb56
suyong choi <dpfprtus@gmail.com>
2024-02-27 22:01:41 +0900

    다시품

--- DP1/구간합구하기5(11660).py
+++ DP1/구간합구하기5(11660).py
@@ -1,22 +1,20 @@
 import sys
 input = sys.stdin.readline
-
 n,m = map(int,input().split())
-
 s = []
-for _ in range(n):
+num = []
+for i in range(n):
     s.append(list(map(int,input().split())))
 
-num = []
-for _ in range(m):
+for i in range(m):
     num.append(list(map(int,input().split())))
 
-dp = [[0] * (n+1) for _ in range(n+1)]
+dp = [[0]*(n+1) for _ in range(n+1)]
 
 for i in range(1,n+1):
     for j in range(1,n+1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1] + s[i-1][j-1] - dp[i-1][j-1]
-                
+        dp[i][j] = dp[i-1][j] +dp[i][j-1]+s[i-1][j-1]-dp[i-1][j-1]
+
 for i in num:
     a,b,c,d = i
-    print(dp[c][d]-dp[a-1][d]-dp[c][b-1]+dp[a-1][b-1])
+    print(dp[c][d] - dp[a-1][d] - dp[c][b-1] + dp[a-1][b-1])
