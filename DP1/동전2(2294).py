10bf1769944ccfa1948f94844aeaa292d049174e
suyong choi <dpfprtus@gmail.com>
2024-03-01 10:44:52 +0900

    또틀림

--- DP1/동전2(2294).py
+++ DP1/동전2(2294).py
@@ -1,12 +1,11 @@
 n,k = map(int,input().split())
 values = [int(input()) for _ in range(n)]
-
 dp = [10001]*(k+1)
-dp[0] = 0  
-for i in values:
-    for j in range(i, k+1):
-        dp[j] = min(dp[j],dp[j-i]+1)
-
+dp[0] = 0
+for value in values:
+    for j in range(value,k+1):
+        if dp[j] > 0:
+            dp[j] = min(dp[j-value]+1,dp[j])
 if dp[k] == 10001:
     print(-1)
 else:
