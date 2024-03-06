0cbaac21d51d05f82034a923bd0eb877df988290
suyong choi <dpfprtus@gmail.com>
2024-03-06 11:50:50 +0900

    다시품

--- DP1/점프(1890).py
+++ DP1/점프(1890).py
@@ -1,19 +1,21 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
-m = []
+s = []
 for _ in range(n):
-    m.append(list(map(int,input().split())))
-dp = [[0]* (n+1) for _ in range(n)]
-dp[0][0] = 1
+    s.append(list(map(int,input().split())))
+dp = [[0]*(n+1) for _ in range(n)]
+dp[0][0] =1
 
 for i in range(n):
     for j in range(n):
         if i == n-1 and j == n-1:
             print(dp[i][j])
             break
-        cur = m[i][j]
-        if i+cur < n:
-            dp[i+cur][j]+= dp[i][j]
-        if j+cur < n:
-            dp[i][j+cur] += dp[i][j]
+        if dp[i][j] != 0 and i + s[i][j]< n:
+            dp[i+s[i][j]][j] += dp[i][j]
+        if dp[i][j] != 0 and j + s[i][j]< n:
+            dp[i][j+s[i][j]] += dp[i][j]
+print(dp)
+print(dp[n-1][n-1])
