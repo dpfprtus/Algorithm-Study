ec5c0ca7566c6707b5f6d87cee694fb362af2bb6
suyong choi <dpfprtus@gmail.com>
2024-03-09 10:40:00 +0900

    또틀림

--- DP1/퇴사2(15486).py
+++ DP1/퇴사2(15486).py
@@ -2,11 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [0] * (n+1)
+dp = [0]*(n+1)
 
 for i in range(1,n+1):
     t,p = map(int,input().split())
-    dp[i] = max(dp[i-1],dp[i])
-    if i+t <= n+1:
-        dp[i+t-1] = max(dp[i+t-1],dp[i-1]+p)
+    dp[i] = max(dp[i],dp[i-1])
+    if t+i <= n+1:
+        dp[t+i-1] = max(dp[t+i-1],dp[i-1]+p)
 print(dp[-1])
