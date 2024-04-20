2062564cb30ae9e7a6e6df96a57629dcd1aaf97a
suyong choi <dpfprtus@gmail.com>
2024-04-20 11:00:31 +0900

    틀림

---
+++ 바킹독/스택/탑(2493).py
@@ -0,0 +1,16 @@
+n = int(input())
+top = list(map(int,input().split()))
+stack = []
+result = [0] * n
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] < top[i]:
+            stack.pop()
+        else:
+            result[i] = stack[-1][0]+1
+            break
+            
+    stack.append((i,top[i]))
+
+print(*result)
