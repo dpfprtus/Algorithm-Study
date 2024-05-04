f6629bc8c9bda31ef57b9d492e0cf7f0bd4a9242
suyong choi <dpfprtus@gmail.com>
2024-05-04 10:36:39 +0900

    정답

---
+++ 바킹독/큐/카드2.py
@@ -0,0 +1,9 @@
+from collections import deque
+n = int(input())
+nums = deque([i for i in range(1,n+1)])
+
+while len(nums) > 1:
+    nums.popleft()
+    a = nums.popleft()
+    nums.append(a)
+print(nums[0])
