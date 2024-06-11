97b1f5af80f70b89a02dc73cd6d64f8ef892061b
suyong choi <dpfprtus@gmail.com>
2024-06-11 09:11:46 +0900

    정답

---
+++ 바킹독/백트래킹/N과M3(15651).py
@@ -0,0 +1,13 @@
+n,m = map(int,input().split())
+
+ans = [0]*m
+
+def func(k):
+    if k == m:
+        print(' '.join(map(str,ans)))
+        return
+    
+    for i in range(1,n+1):
+        ans[k] = i
+        func(k+1)
+func(0)
