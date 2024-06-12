17a76dbd43bd59a16660224a1d4b9160f38a310b
suyong choi <dpfprtus@gmail.com>
2024-06-12 11:51:07 +0900

    정답

---
+++ 바킹독/백트래킹/N과M6(15655).py
@@ -0,0 +1,19 @@
+n,m = map(int,input().split())
+nums = list(map(int,input().split()))
+nums.sort()
+ans = [0]*m
+istrue = [0]*(10001)
+
+def func(k):
+    
+    if k == m:
+        print(' '.join(map(str,ans)))
+        return
+
+    for i in nums:
+        if k !=0 and ans[k-1] >= i:
+            continue
+        ans[k] = i
+        func(k+1)
+        
+func(0)
