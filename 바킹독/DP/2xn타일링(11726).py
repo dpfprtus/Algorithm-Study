e9410d501363df9808222b35bffba13cad3939db
dpfprtus <dpfprtus@gmail.com>
2024-06-23 15:21:33 +0900

    정답

--- 바킹독/DP/2xn타일링(11726).py
+++ 바킹독/DP/2xn타일링(11726).py
@@ -6,5 +6,6 @@ dp[2] = 2
 dp[3] = 3
 
 for i in range(4,n+1):
-    dp[i] = dp[i-1]+dp[i-2]
-print(dp[n] % 10007)
+    #오버플로가 발생할 수 있으니 계속 나머지 연산을 해줘야함.
+    dp[i] = dp[i-1]+dp[i-2] % 10007
+print(dp[n])
